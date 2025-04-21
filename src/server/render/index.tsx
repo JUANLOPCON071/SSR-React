21c5d7d1c30d50ae8f26804698ae011e16150caa
juanlopcon07 <juan.21futmas07@gmail.com>
2025-04-20 21:09:45 -0500

    Initial Props y Consultas a la API desde el servidor

--- src/server/render/index.tsx
+++ src/server/render/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { App } from "../../app/containers/App";
-import { renderToString } from 'react-dom/server'
 import { StaticRouter } from "react-router-dom/server";
+import { renderToString } from 'react-dom/server'
 import { template } from "./template";
 
-export const render = (url: string) => {
+export const render = (url: string, initialProps={}) => {
     const stream = renderToString(
         <StaticRouter location={url}>
             <App/>
         </StaticRouter>
     )
 
-    const html = template(stream)
+    const html = template(stream, initialProps)
 
     return html
 }
